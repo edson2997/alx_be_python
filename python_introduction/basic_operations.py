f1d1ad453ecbd0efc50e25032214f5fa685e2021
Edson Morgado <edsonmorgadoboane15@gmail.com>
2025-07-20 13:33:51 +0200

    Improved code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,5 @@
-number_1 = 10 
-number_2 = 5
+number1 = 10 
+number2 = 5
 print ("Addition of {} and {} is {}".format(number_1, number_2, number_1 + number_2))
 print ("Subtraction of {} and {} is {}".format(number_1, number_2, number_1 - number_2))
 print ("Multiplication of {} and {} is {}".format(number_1, number_2, number_1 * number_2))
