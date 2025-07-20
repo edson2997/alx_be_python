75e961cfbc03c798ebab60289807e6ef0d1cbf40
Edson Morgado <edsonmorgadoboane15@gmail.com>
2025-07-20 13:49:07 +0200

    Corrected code mistakes

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,9 @@
 number1 = 10 
 number2 = 5
-print ("Addition of {} and {} is {}".format(number_1, number_2, number_1 + number_2))
-print ("Subtraction of {} and {} is {}".format(number_1, number_2, number_1 - number_2))
-print ("Multiplication of {} and {} is {}".format(number_1, number_2, number_1 * number_2))
+print ("Addition of {} and {} is {}".format(number1, number2, number1 + number2))
+print ("Subtraction of {} and {} is {}".format(number1, number2, number1 - number2))
+print ("Multiplication of {} and {} is {}".format(number1, number2, number1 * number2))
+
+
 
 
